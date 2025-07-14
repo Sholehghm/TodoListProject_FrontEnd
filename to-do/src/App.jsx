435f827e6789260107c741f465f87d94183bba64
Aref <aref.dehghanimo@gmail.com>
2025-07-13 22:32:38 -0700

    install vite and tailwind and material UI

---
+++ to-do/src/App.jsx
@@ -0,0 +1,13 @@
+import Button from '@mui/material/Button'
+
+function App() {
+
+  return (
+    <>
+     <h1 className='bg-gray-300'>to do</h1>
+     <Button>click me</Button>
+    </>
+  )
+}
+
+export default App
