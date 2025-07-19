7a94d33d312123b3f0ae5aa91f2ac63f452d03d3
Aref <aref.dehghanimo@gmail.com>
2025-07-18 22:57:09 -0700

    add breadCrumb component

--- to-do/src/App.jsx
+++ to-do/src/App.jsx
@@ -1,11 +1,10 @@
-import Button from '@mui/material/Button'
+import BasicBreadcrumb from './components/breadcrumbs/BasicBreadCrumb'
 
 function App() {
 
   return (
     <>
-     <h1 className='bg-gray-300'>to do</h1>
-     <Button>click me</Button>
+     <BasicBreadcrumb path="aref"/>
     </>
   )
 }
