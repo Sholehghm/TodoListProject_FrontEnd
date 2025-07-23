fd88e208571f907362235794918f97b069785f68
Aref <aref.dehghanimo@gmail.com>
2025-07-22 22:17:50 -0700

    modify tasklist

--- to-do/src/components/list/TaskList.jsx
+++ to-do/src/components/list/TaskList.jsx
@@ -1,9 +1,23 @@
-import Box from "@mui/material";
+import React from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import Task from "../task/Task";
+import TaskBox from "./TaskBox";
 
-export default function TaskList(){
+export default function TaskList({tasks}){
     return(
-        <Box>
-            
+        <>
+        <Box className='flex-1 '>
+        <Box className='flex fles-row justify-around  flex-wrap'>
+            <TaskBox status='to do' tasks={tasks}/>
+            <TaskBox status='in progress' tasks={tasks}/>
+            <TaskBox status='completed' tasks={tasks}/>
         </Box>
+        </Box>
+        
+
+        </>
+    
     )
-}
+};
