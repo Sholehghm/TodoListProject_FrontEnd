6f71e0f63f8ab5b7875cf44eb56d47a5ff5639a0
Aref <aref.dehghanimo@gmail.com>
2025-07-22 22:17:07 -0700

    add mockdata props

--- to-do/src/App.jsx
+++ to-do/src/App.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Home from './components/pages/Home'
-
+import data from './mockdata/MockData.json'
 
 function App() {
-
+  const[tasks,setTask]=useState(data);
   return (
     <>
-    <Home/>
+    <Home tasks={tasks}/>
     </>
   )
 }
