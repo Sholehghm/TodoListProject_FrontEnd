66b843baf29cb59e37f62ae6fa265678274cc9a4
Aref <aref.dehghanimo@gmail.com>
2025-07-22 22:18:08 -0700

    modify nome page

--- to-do/src/components/pages/Home.jsx
+++ to-do/src/components/pages/Home.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import ResponsiveAppBar from "../navbar/NavBar";
 import AddEditForm from "../form/AddEditForm";
+import TaskList from "../list/TaskList";
 
 
-export default function Home(){
+export default function Home({tasks}){
 return(
     <div className='sm:flex sm:flex-row '>
         <ResponsiveAppBar/>
-        
-        <AddEditForm mode='add' />
+        <TaskList tasks={tasks}/>
+        {/* <AddEditForm mode='add' /> */}
      
     </div>
 )
