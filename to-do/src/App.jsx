1ea906809fd32fdbca0277a8a39e9ed48d3da52b
Aref <aref.dehghanimo@gmail.com>
2025-07-23 20:19:10 -0700

    add routs

--- to-do/src/App.jsx
+++ to-do/src/App.jsx
@@ -1,12 +1,21 @@
 import React, { useState } from 'react'
 import Home from './components/pages/Home'
 import data from './mockdata/MockData.json'
+import { BrowserRouter as Router, Routes, Route } from "react-router";
 
 function App() {
   const[tasks,setTask]=useState(data);
   return (
     <>
-    <Home tasks={tasks}/>
+    <Router>
+      <Routes>
+        <Route index element={<Home tasks={tasks}/>}/>
+        <Route path='/home' element={<Home tasks={tasks}/>}/>
+        <Route path='/registration' element={<Home tasks={tasks}/>}/>
+        <Route path='/today tasks' element={<Home tasks={tasks}/>}/>
+        <Route path='/calendar' element={<Home tasks={tasks}/>}/>
+      </Routes>
+    </Router>
     </>
   )
 }
