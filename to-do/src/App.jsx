2babd3b291af2d6798262b03ed2c677b0a867349
Aref <aref.dehghanimo@gmail.com>
2025-07-24 23:30:10 -0700

    change add task route component

--- to-do/src/App.jsx
+++ to-do/src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import Home from './components/pages/Home'
 import data from './mockdata/MockData.json'
 import { BrowserRouter as Router, Routes, Route } from "react-router";
+import AddTask from './components/pages/AddTask';
 
 function App() {
   const[tasks,setTask]=useState(data);
@@ -13,7 +14,7 @@ function App() {
         <Route path='/home' element={<Home tasks={tasks}/>}/>
         <Route path='/registration' element={<Home tasks={tasks}/>}/>
         <Route path='/today tasks' element={<Home tasks={tasks}/>}/>
-        <Route path='/Add task' element={<Home tasks={tasks}/>}/>
+        <Route path='/Add task' element={<AddTask tasks={tasks}/>}/>
         <Route path='/calendar' element={<Home tasks={tasks}/>}/>
       </Routes>
     </Router>
