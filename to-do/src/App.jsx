2cb5c3a4098813c042f177df1da48021e3ad8963
Aref <aref.dehghanimo@gmail.com>
2025-07-25 18:38:30 -0700

    sent set tasks to add task component

--- to-do/src/App.jsx
+++ to-do/src/App.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router";
 import AddTask from './components/pages/AddTask';
 
 function App() {
-  const[tasks,setTask]=useState(data);
+  const[tasks,setTasks]=useState(data);
   return (
     <>
     <Router>
@@ -14,7 +14,7 @@ function App() {
         <Route path='/home' element={<Home tasks={tasks}/>}/>
         <Route path='/registration' element={<Home tasks={tasks}/>}/>
         <Route path='/today tasks' element={<Home tasks={tasks}/>}/>
-        <Route path='/Add task' element={<AddTask tasks={tasks}/>}/>
+        <Route path='/Add task' element={<AddTask tasks={tasks} setTasks={setTasks}/>}/>
         <Route path='/calendar' element={<Home tasks={tasks}/>}/>
       </Routes>
     </Router>
