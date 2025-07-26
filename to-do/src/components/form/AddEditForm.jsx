abd91c5224c38d8e4294c098e032f5470acdb752
Aref <aref.dehghanimo@gmail.com>
2025-07-25 18:39:25 -0700

    complete add mode of form

--- to-do/src/components/form/AddEditForm.jsx
+++ to-do/src/components/form/AddEditForm.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import TextField from "@mui/material/TextField";
 import Textarea from '@mui/joy/Textarea';
 import BasicDatePicker from "./BasicDatePicker";
@@ -7,8 +7,26 @@ import BasicSelect from "./BasicSelect";
 import Button from "@mui/material/Button";
 import AddIcon from "@mui/icons-material/Add";
 import Box from "@mui/material/Box";
+import {v4 as uuid4} from "uuid";
+import dayjs from 'dayjs';
 
-export default function AddEditForm({ mode = 'add' }) {
+export default function AddEditForm({ mode = 'add',task,tasks,setTasks}) {
+    const[title,setTitle]=useState('');
+    const[description,setDescription]=useState('');
+    const[date,setDate]=useState(null);
+    const[status,setStatus]=useState('');
+
+    const addTask = () =>{
+        const newTask = {
+            id: uuid4(),
+            title,
+            description,
+            date: date ? date.format('YYYY-MM-DD') : '',
+            status
+        }
+    setTasks([...tasks, newTask]);
+    };
+    const editTask = () =>{};
     return (
         <Box className='flex-1'>
             <div className="flex h-screen items-center justify-center">
@@ -19,15 +37,19 @@ export default function AddEditForm({ mode = 'add' }) {
                 <TextField
                     label="title"
                     variant="outlined"
-                    className="w-full" />
+                    className="w-full"
+                    onChange={(e)=>{setTitle(e.target.value)}}
+                    value={title}/>
                 <Textarea
                     size="lg"
                     name="description"
                     placeholder="Description"
-                    minRows={4} />
-                <BasicDatePicker />
-                <BasicSelect />
-                <Button variant="contained">
+                    minRows={4}
+                    onChange={(e)=>{setDescription(e.target.value)}}
+                    value={description} />
+                <BasicDatePicker value={date} onChange={setDate} />
+                <BasicSelect value={status} onChange={(e) =>{setStatus(e.target.value)}} />
+                <Button variant="contained" onClick={mode==='add'? addTask:editTask}>
                     <AddIcon />
                     {mode}
                 </Button>
